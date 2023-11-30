c7499c07cbec13ed7162ed564b14ebc6ad82b95e
Douglas Martinez <douggmartinez12@gmail.com>
2023-11-29 22:28:07 -0600

    add link

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,29 +62,6 @@ router.post('/signup', async (req, res) => {
   }
 });
 
-// // GET route to render the edit profile page
-// router.get('/editProfile', (req, res) => {
-//   // Render the editProfile.ejs template
-//   res.render('editProfile');
-// });
-
-// // POST route to handle form submission and update the profile
-// router.post('/profile', async (req, res) => {
-//   const { name } = req.body;
-
-//   try {
-//     // Assuming you have a user model, update the user's name
-//     req.user.name = name;
-//     await req.user.save();
-
-//     // Redirect back to the profile page after successful update
-//     res.redirect('/profile');
-//   } catch (error) {
-//     // Handle the update failure, you might want to send an error response
-//     console.error(error);
-//     res.status(500).json({ success: false, message: 'Failed to update profile' });
-//   }
-// });
 
 
 
